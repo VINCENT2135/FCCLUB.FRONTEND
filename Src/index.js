a508603f8a7c2740711595cbfa5a1c7f2dd53a3b
Vincent Ivos <vincentivos1@gmail.com>
2021-05-05 19:57:33 -0400

    initial fetch

---
+++ Src/index.js
@@ -0,0 +1,40 @@
+ 
+
+
+document.querySelector("#todos").addEventListener("click", function(event) {
+
+ var td = event.target;
+
+
+})  
+
+
+function fetchClubs()
+
+{
+
+  const th = document.getElementById("")
+
+   th.innerHTML = "Club Names"
+
+ 
+
+  fetch("http://localhost:3000/lists")
+
+  .then(r => r.json())
+
+
+
+  .then(appendLists)
+
+}
+
+
+document.querySelector("#todos").addEventListener("click", function(event) {
+
+    var td = event.target;
+
+
+})  
+
+
