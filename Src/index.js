7648b74804634824e39b1a2b2e220b8e61423b5e
Vincent Ivos <vincentivos1@gmail.com>
2021-05-06 13:19:21 -0400

    Fetch

--- Src/index.js
+++ Src/index.js
@@ -1,40 +1,93 @@
+
+
  
 
+document.querySelector("#clubs").addEventListener("click", function(event) {
 
-document.querySelector("#todos").addEventListener("click", function(event) {
+  var td = event.target;
 
- var td = event.target;
+ 
+
+  if ( td.matches("button"))
+
+  {             
+
+   
+  
+ 
+
+               if (td.innerHTML == "Todo"){      
 
 
-})  
 
+      fetchClubs(td.value)
+
+     }
+
+ 
+
+    if (td.innerHTML == "Add" && td.value == "List")
+
+    {         
+
+        alert("add Club td.value =" + td.value  ) 
+
+       addClub()
+
+     }
+
+              
+
+    if (td.innerHTML == "Add" && td.value != "Club")
+
+    {         
+
+        alert("add Todo td.value =" + td.value  ) 
+
+       addPlayer(td.value)
+
+    }
+
+              
+
+               if (td.innerHTML == "Delete" )
+
+    {         
+
+ 
+
+     //   alert("delete List td.value =" + td.value  ) 
+
+       deleteTodo(td.value)
+
+     }
+
+              
+
+  }
+
+});
+
+ 
 
 function fetchClubs()
 
 {
 
-  const th = document.getElementById("")
+  const th = document.getElementById("th0")
 
-   th.innerHTML = "Club Names"
+   th.innerHTML = "Clubs"
 
  
 
-  fetch("http://localhost:3000/lists")
+  fetch("http://localhost:3000/clubs")
 
   .then(r => r.json())
 
-
+  //  .then(r => appendLists(r))  or the next
 
   .then(appendLists)
 
 }
 
-
-document.querySelector("#todos").addEventListener("click", function(event) {
-
-    var td = event.target;
-
-
-})  
-
-
+ 
