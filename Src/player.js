a8943ff7616a871ea24518318dc3db510db834c3
VINCENT2135 <68698502+VINCENT2135@users.noreply.github.com>
2021-05-09 14:30:29 -0400

    Refactored Fetch into Class

--- Src/player.js
+++ Src/player.js
@@ -1,3 +1,25 @@
+class Player {
+	constructor() {
+
+	}
 
 
+	fetchPlayers(club) {
+		const th0 = document.getElementById("th0")
+		const name = document.getElementById(`clubname${club}`)
+		th0.innerHTML = "Players for : " + name.value
 
+		const th99 = document.getElementById("headrow")
+		th99.deleteCell(4);
+
+		fetch("http://localhost:3000/players/" + club)
+			.then(r => r.json())
+		
+			.then(data => {
+				return this.appendPlayers(data, club)
+			})
+		
+
+	}
+  
+ 
