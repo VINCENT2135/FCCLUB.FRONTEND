84d22b770b9989f378858457f17ed84ea524d913
VINCENT2135 <68698502+VINCENT2135@users.noreply.github.com>
2021-05-09 19:11:40 -0400

    InsertPlayer

--- Src/player.js
+++ Src/player.js
@@ -23,3 +23,27 @@ class Player {
 	}
   
  
+insertPlayer(player) {
+		const playerRow =
+			`
+  <tr id="trplayer${player.id}">
+  <td><input id="playername${player.id}" value="${player.playername}" type="text"></td>
+	 <td></td>
+  <td><button name="Player" value="${player.id}" type="button">Update</button></td>
+  <td><button name="Player" value="${player.id}" type="button">Delete</button></td>
+  </tr>
+  `
+		return playerRow
+	}
+
+	insertPlayerAdd(club_id) {
+		const playerRow =
+			`   
+   <tr id="trplayer999">
+   <td><input id="playername999" value="" type="text"></td>
+   <td><button id="add999" name="Player" value="${club_id}" type="button">Add</button></td>
+   <td colspan="2" style="text-align:center"><button name="ShowClub" type="button">Show Clubs</button></td>
+   </tr>
+  `
+		return playerRow
+	}
